f160ad47f8dafe5f0938c8a12e03044f724ca56b
Qingyu Wang <wangqingyu.c0l1n@bytedance.com>
2026-02-27 15:39:09 +0800

    test: verify recompilation when theme file changes

---
+++ test/recompilation/src/index.js
@@ -0,0 +1,7 @@
+import './index.css';
+
+const div = document.createElement('div');
+div.id = 'dynamic';
+div.className = 'dynamic-bg';
+div.textContent = 'Dynamic';
+document.body.appendChild(div);
